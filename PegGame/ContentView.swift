ce27acee25b11fce53838dfbfb8f6e345aa92aef
Kody Deda <kodydeda4@gmail.com>
2023-09-22 16:49:12 -0400

    init

--- PegGame/ContentView.swift
+++ PegGame/ContentView.swift
@@ -1,24 +1,40 @@
-//
-//  ContentView.swift
-//  PegGame
-//
-//  Created by Kody Deda on 9/22/23.
-//
-
 import SwiftUI
+import ComposableArchitecture
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+struct AppView: View {
+  struct Node: Identifiable, Equatable {
+    var id: [Int] { [row, col] }
+    let row: Int
+    let col: Int
+    var done = false
+  }
+  
+  @State var pegs = IdentifiedArrayOf<Node>(uniqueElements: (0..<5).map { row in (0..<row+1).map { Node(row: row, col: $0) } }.flatMap { $0 })
+  @State var selection: Node? = nil
+  
+  private func pegView(peg: Node) -> some View {
+    Button(action: { selection = peg }) {
+      Circle()
+        .frame(width: 30, height: 30)
+        .foregroundColor(selection == peg ? .accentColor : .primary)
     }
+    .buttonStyle(.plain)
+    .animation(.default, value: selection)
+  }
+  
+  var body: some View {
+    VStack {
+      ForEach(0..<5) { row in
+        HStack {
+          ForEach(0..<row+1) { col in
+            pegView(peg: pegs[id: [row, col]]!)
+          }
+        }
+      }
+    }
+  }
 }
 
 #Preview {
-    ContentView()
+  AppView()
 }
