55a1dfd19f1fd8f785dbfbe0c61ab79ab300a67a
Kody Deda <kodydeda4@gmail.com>
2023-09-22 17:30:43 -0400

    Update ContentView.swift

--- PegGame/ContentView.swift
+++ PegGame/ContentView.swift
@@ -1,64 +1,120 @@
 import SwiftUI
 import ComposableArchitecture
 
-struct AppView: View {
-  
-  struct Peg: Identifiable, Equatable {
-    var id: [Int] { [row, col] }
-    let row: Int
-    let col: Int
-    var done = false
+struct AppReducer: Reducer {
+  struct State: Equatable {
+    @BindingState var pegs = IdentifiedArrayOf<Peg>(
+      uniqueElements: (0..<5).map { row in
+        (0..<row+1).map {
+          Peg(row: row, col: $0)
+        }
+      }.flatMap {
+        $0
+      }
+    )
+    @BindingState var selection: Peg? = nil
   }
   
-  @State var pegs = IdentifiedArrayOf<Peg>(uniqueElements: (0..<5).map { row in (0..<row+1).map { Peg(row: row, col: $0) } }.flatMap { $0 })
-  @State var selection: Peg? = nil
+  enum Action: BindableAction, Equatable {
+    case pegTapped(Peg)
+    case binding(BindingAction<State>)
+  }
   
-  private func isSelectable(_ peg: Peg) -> Bool {
-    guard let selection = selection else { return true }
-    return [
+  var body: some ReducerOf<Self> {
+    BindingReducer()
+    Reduce { state, action in
+      switch action {
+      
+      case let .pegTapped(value):
+        if let s = state.selection {
+          //pegs[id: [s.row - 1, s.col - 1]]?.done.toggle()
+          state.selection = nil
+        } else {
+          state.selection = state.selection != value ? value : nil
+        }
+        return .none
+        
+      case .binding:
+        return .none
+        
+      }
+    }
+  }
+}
+
+extension AppReducer.State {
+  var options: IdentifiedArrayOf<Peg> {
+    guard let selection = selection else { return [] }
+    
+    return .init(uniqueElements: [
       pegs[id: [selection.row+2, selection.col]],
       pegs[id: [selection.row-2, selection.col]],
       pegs[id: [selection.row, selection.col+2]],
       pegs[id: [selection.row, selection.col-2]],
-    ]
-      .compactMap { $0 }
-      .contains(peg)
+    ].compactMap { $0 })
   }
-  
-  
+}
+
+// MARK: - SwiftUI
+
+struct Peg: Identifiable, Equatable {
+  var id: [Int] { [row, col] }
+  let row: Int
+  let col: Int
+  var completed = false
+}
+
+// MARK: - SwiftUI Previews
+
+struct AppView: View {
+  let store: StoreOf<AppReducer> = Store(
+    initialState: AppReducer.State(),
+    reducer: AppReducer.init
+  )
   
   private func pegView(peg: Peg) -> some View {
-    Button(action: {
-      if let s = selection {
-        pegs[id: [s.row - 1, s.col - 1]]?.done.toggle()
-        selection = nil
-      } else {
-        selection = selection != peg ? peg : nil
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      Button(action: { viewStore.send(.pegTapped(peg)) }) {
+        Circle()
+          .frame(width: 50, height: 50)
+          .foregroundColor(viewStore.selection == peg ? .accentColor : peg.completed ? Color.red.opacity(0.5) : .primary)
+          .opacity(viewStore.selection == nil || viewStore.selection == peg ? 1 : 0.5)
+          .opacity(viewStore.options.isEmpty || viewStore.options.contains(peg) ? 1 : 0.5)
       }
-    }) {
-      Circle()
-        .frame(width: 50, height: 50)
-        .foregroundColor(selection == peg ? .accentColor : peg.done ? Color.red.opacity(0.5) : .primary)
-        .opacity(selection == nil || selection == peg ? 1 : 0.5)
-        .opacity(isSelectable(peg) ? 1 : 0.5)
+      .buttonStyle(.plain)
+      .animation(.default, value: viewStore.selection)
     }
-    .buttonStyle(.plain)
-    .animation(.default, value: selection)
   }
   
   var body: some View {
-    NavigationStack {
-      VStack {
-        ForEach(0..<5) { row in
-          HStack {
-            ForEach(0..<row+1) { col in
-              pegView(peg: pegs[id: [row, col]]!)
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      NavigationStack {
+        VStack {
+          ForEach(0..<5) { row in
+            HStack {
+              ForEach(0..<row+1) { col in
+                pegView(peg: viewStore.pegs[id: [row, col]]!)
+              }
             }
           }
         }
+        .navigationTitle("Peg Game")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+          ToolbarItem(placement: .navigationBarLeading) {
+            Button(action: {}) {
+              VStack {
+                HStack {
+                  Text("Undo")
+                  Image(systemName: "arrow.uturn.backward")
+                }
+              }
+            }
+            .disabled(true)
+            .buttonStyle(.bordered)
+          }
+        }
       }
-      .navigationTitle("Peg Game")
-      .navigationBarTitleDisplayMode(.inline)
     }
   }
 }
