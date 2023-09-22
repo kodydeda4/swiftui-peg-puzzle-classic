118ed6dc009d4434f61f3fff835d97d9f8850205
Kody Deda <kodydeda4@gmail.com>
2023-09-22 17:06:34 -0400

    Update ContentView.swift

--- PegGame/ContentView.swift
+++ PegGame/ContentView.swift
@@ -2,35 +2,63 @@ import SwiftUI
 import ComposableArchitecture
 
 struct AppView: View {
-  struct Node: Identifiable, Equatable {
+  
+  struct Peg: Identifiable, Equatable {
     var id: [Int] { [row, col] }
     let row: Int
     let col: Int
     var done = false
   }
   
-  @State var pegs = IdentifiedArrayOf<Node>(uniqueElements: (0..<5).map { row in (0..<row+1).map { Node(row: row, col: $0) } }.flatMap { $0 })
-  @State var selection: Node? = nil
+  @State var pegs = IdentifiedArrayOf<Peg>(uniqueElements: (0..<5).map { row in (0..<row+1).map { Peg(row: row, col: $0) } }.flatMap { $0 })
+  @State var selection: Peg? = nil
   
-  private func pegView(peg: Node) -> some View {
-    Button(action: { selection = peg }) {
+  private func isSelectable(_ peg: Peg) -> Bool {
+    guard let selection = selection else { return true }
+    return [
+      pegs[id: [selection.row+2, selection.col]],
+      pegs[id: [selection.row-2, selection.col]],
+      pegs[id: [selection.row, selection.col+2]],
+      pegs[id: [selection.row, selection.col-2]],
+    ]
+      .compactMap { $0 }
+      .contains(peg)
+  }
+  
+  
+  
+  private func pegView(peg: Peg) -> some View {
+    Button(action: {
+      if let s = selection {
+        pegs[id: [s.row - 1, s.col - 1]]?.done.toggle()
+        selection = nil
+      } else {
+        selection = selection != peg ? peg : nil
+      }
+    }) {
       Circle()
-        .frame(width: 30, height: 30)
-        .foregroundColor(selection == peg ? .accentColor : .primary)
+        .frame(width: 50, height: 50)
+        .foregroundColor(selection == peg ? .accentColor : peg.done ? Color.red.opacity(0.5) : .primary)
+        .opacity(selection == nil || selection == peg ? 1 : 0.5)
+        .opacity(isSelectable(peg) ? 1 : 0.5)
     }
     .buttonStyle(.plain)
     .animation(.default, value: selection)
   }
   
   var body: some View {
-    VStack {
-      ForEach(0..<5) { row in
-        HStack {
-          ForEach(0..<row+1) { col in
-            pegView(peg: pegs[id: [row, col]]!)
+    NavigationStack {
+      VStack {
+        ForEach(0..<5) { row in
+          HStack {
+            ForEach(0..<row+1) { col in
+              pegView(peg: pegs[id: [row, col]]!)
+            }
           }
         }
       }
+      .navigationTitle("Peg Game")
+      .navigationBarTitleDisplayMode(.inline)
     }
   }
 }
