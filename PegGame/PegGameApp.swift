4fcc4f865fe7349abd2c7d4b7bc92096eeb2152c
Kody Deda <kodydeda4@gmail.com>
2023-09-22 16:04:03 -0400

    Initial Commit

---
+++ PegGame/PegGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PegGameApp.swift
+//  PegGame
+//
+//  Created by Kody Deda on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct PegGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
